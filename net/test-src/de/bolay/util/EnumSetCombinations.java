cd692d36ab1f89b94fe3b59c1a2bd5f2c92ff4d2
kai <kai@bolay.de>
2008-11-20 06:49:53 +0000

    refactor Iterator to be top-level

--- net/test-src/de/bolay/util/EnumSetCombinations.java
+++ net/test-src/de/bolay/util/EnumSetCombinations.java
@@ -5,44 +5,15 @@ import java.util.Iterator;
 
 public class EnumSetCombinations<T extends Enum<T>>
     implements Iterable<EnumSet<T>> {
+
   private final Class<T> type;
 
-  public EnumSetCombinations(final Class<T> type) {
+  public EnumSetCombinations(Class<T> type) {
     this.type = type;
   }
 
-  private class EnumSetCombinationIterator implements Iterator<EnumSet<T>> {
-    T[] values = type.getEnumConstants();
-    EnumSet<T> nextSet = EnumSet.noneOf(type);
-
-    public boolean hasNext() {
-      return nextSet != null;
-    }
-
-    public EnumSet<T> next() {
-      EnumSet<T> retval = nextSet.clone();
-      updateNext();
-      return retval;
-    }
-
-    void updateNext() {
-      for (T value : values) {
-        if (nextSet.contains(value)) {
-          nextSet.remove(value);
-        } else {
-          nextSet.add(value);
-          return;
-        }
-      }
-      nextSet = null;
-    }
-
-    public void remove() {
-      throw new UnsupportedOperationException();
-    }
-  }
-
   public Iterator<EnumSet<T>> iterator() {
-    return new EnumSetCombinationIterator();
+    return new EnumSetCombinationIterator<T>(
+        type.getEnumConstants(), EnumSet.noneOf(type));
   }
 }
